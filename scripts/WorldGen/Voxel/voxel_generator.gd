1141897af6f7cbb1d4c4dea7f54f7be7e3cc7bdb
forzinkahd <57588821+forzinkahd@users.noreply.github.com>
2026-02-07 22:31:14 +0100

    probably just graphic bug in editor due to comments

--- scripts/WorldGen/Voxel/voxel_generator.gd
+++ scripts/WorldGen/Voxel/voxel_generator.gd
@@ -159,14 +159,14 @@ func shape_geometry(prism) -> bool:
 	# Terrace shaping
 	if settings.terrace_steps >= 1:
 		push_warning("Terrace shaping code is disabled and may be obsolete")
-		"""var table = VoxelData.get_tile_neighbor_table(prism.grid_position_xz.x)
+		var table = VoxelData.get_tile_neighbor_table(prism.grid_position_xz.x)
 		for dir in table:
 			var neighbor_pos = Vector3i(prism.grid_position_xyz.x + dir.x,
 										prism.grid_position_xyz.y - settings.terrace_steps,
 										prism.grid_position_xyz.z + dir.y)
 			if air_at_pos(neighbor_pos):
 				prism.type = VoxelData.voxel_type.AIR
-				return true"""
+				return true
 	
 	return false
 
