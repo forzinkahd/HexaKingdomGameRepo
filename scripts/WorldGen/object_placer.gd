d5f75f40a170f3ae78d9eb89e40e841ca646c312
forzinkahd <57588821+forzinkahd@users.noreply.github.com>
2026-02-10 22:04:29 +0100

    deleted unused scenes

--- scripts/WorldGen/object_placer.gd
+++ scripts/WorldGen/object_placer.gd
@@ -3,8 +3,7 @@ class_name ObjectPlacer
 
 # from Prototype, either rework for props or delete at some point 10.02.2026
 
-@export var village : PackedScene
-@export var proto_unit : PackedScene
+
 
 
 # Spawn an object on a tile
