319055aadfc9b6171ba289c3a73b089bf851dac8
forzinkahd <57588821+forzinkahd@users.noreply.github.com>
2026-06-22 18:09:34 +0200

    Intro panel

---
+++ scripts/UIscripts/dismiss_panel.gd
@@ -0,0 +1,27 @@
+class_name DismissiblePanel
+extends Control
+
+# Use for any panel that needs to disappear but can be reopened.
+
+signal dismissed
+
+@export var dismiss_button: Button
+@export var free_on_dismiss: bool = false
+
+
+func _ready() -> void:
+	if dismiss_button != null:
+		dismiss_button.pressed.connect(dismiss)
+
+
+func dismiss() -> void:
+	dismissed.emit()
+
+	if free_on_dismiss:
+		queue_free()
+	else:
+		hide()
+
+
+func open() -> void:
+	show()
