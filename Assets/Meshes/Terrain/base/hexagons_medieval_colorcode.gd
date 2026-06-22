0f6dcfb090a0b70cd6a0d1a8f3b4bc02dea69823
forzinkahd <57588821+forzinkahd@users.noreply.github.com>
2026-06-22 21:21:25 +0200

    declutter

---
+++ Assets/Meshes/Terrain/base/hexagons_medieval_colorcode.gd
@@ -0,0 +1,11 @@
+extends Node
+
+const UI_PANEL_BG := Color8(16, 19, 28, 215)
+const UI_PANEL_BG_LIGHT := Color8(81, 75, 72, 205)
+const UI_BORDER := Color8(221, 190, 149, 255)
+const UI_BORDER_HOVER := Color8(231, 175, 106, 255)
+const UI_TEXT := Color8(236, 235, 215, 255)
+const UI_TEXT_MUTED := Color8(212, 169, 122, 255)
+const UI_WARNING := Color8(218, 79, 65, 255)
+const UI_SUCCESS := Color8(0, 109, 79, 255)
+const UI_INFO := Color8(98, 174, 175, 255)
